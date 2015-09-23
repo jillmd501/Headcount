0baf6e8d9b661d7b0bd2418d6296dd684bed524a
Marlo Major <MarloMajor@yahoo.com>
2015-09-22 21:50:34 -0600

    Created district instances

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,27 +1,17 @@
-require_relative 'districts'
-require 'csv'
+require_relative 'districts'  # => true
+require 'csv'                 # => true
 
 class DistrictRepository
   def initialize(districts)
-    @districts = {}
-  end
-
-  # def self.from_csv(path)
-  #   file_name = File.readlines "Students qualifying for free or reduced price lunch"
-  #   districts = {}
-  #   file_name.each do |row|
-  #     columns = file_name.split(",")
-  #     districts << column[0]
-  #   end
-  #   DistrictRepository.new(districts)
-  # end
+    @districts = districts
+  end                        # => :initialize
 
   def self.from_csv(path)
-    file_name = CSV.read "/Users/marlomajor/code/headcount/data/Students qualifying for free or reduced price lunch.csv", headers: true, header_converters: :symbol
-    columns = file_name.split(",")
-    @districts << columns[0]
-    p districts
-  end
+    rows = CSV.read "/Users/marlomajor/code/headcount/data/Students qualifying for free or reduced price lunch.csv", headers: true, header_converters: :symbol
+    districts = rows.map {|district| district.to_h}
+    repository = DistrictRepository.new(districts)
+    # require 'pry';binding.pry
+  end                                                                                                                                                           # => :from_csv
 
   def student_qualify_free_lunch_time_and_data
     districts = {}
@@ -31,9 +21,10 @@ class DistrictRepository
       timeframe = row[:TimeFrame]
       data = row[:Data]
     end
-  end
+  end                                                                                                                         # => :student_qualify_free_lunch_time_and_data
 
   def find_by_name(name)
-    Districts.new(district)
-  end
-end
+    # require 'pry';binding.pry
+
+  end                          # => :find_by_name
+end                            # => :find_by_name
