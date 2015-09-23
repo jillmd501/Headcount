02ecd562a692356406918d567bcd9c08e06b2af9
Jill Donahue <jillmd501@gmail.com>
2015-09-22 18:35:45 -0600

    Remove irrelevant test methods

--- test/test_enrollment.rb
+++ test/test_enrollment.rb
@@ -5,29 +5,4 @@ require './lib/enrollment'
 require './lib/district_repository'
 
 class TestEnrollment < Minitest::Test
-
-  def test_free_or_reduced_lunch_by_year
-    skip
-  end
-
-  def test_free_or_reduced_lunch_in_year(year)
-    skip
-  end
-
-  def test_school_aged_children_in_poverty_in_year
-    skip
-  end
-
-  def test_school_aged_children_in_poverty_in_year(year)
-    skip
-  end
-
-  def test_title_1_students_by_year
-    skip
-  end
-
-  def test_title_1_students_by_year(year)
-    skip
-  end
-
 end
