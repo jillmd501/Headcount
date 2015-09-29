ed038605d0466fb7306869ec42a4f48ccdc60b02
Marlo Major <MarloMajor@yahoo.com>
2015-09-28 19:47:24 -0600

    adding other tests and features

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,24 +1,40 @@
-require 'pry'                           # => true, false
-require 'csv'                           # => true, false
-require_relative 'district_repository'  # => false, true
-require_relative 'districts'            # => false, false
-require_relative 'file_names'           # => true, false
+require 'pry'
+require 'csv'
+require_relative 'district_repository'
+require_relative 'districts'
+require_relative 'file_names'
 
 class EconomicProfile
-  attr_reader :data    # => nil, nil
+  attr_reader :data
 
   def initialize(districts, data)
     @districts = districts
     @data = data
-  end                              # => :initialize, :initialize
+  end
 
   def free_or_reduced_lunch_in_year(year)
     timeframe  = data.select{|row| row.fetch(:timeframe) == year.to_s && row.fetch(:poverty_level) == "Eligible for Free or Reduced Lunch"}
-    timeframe.find {|thing| thing.fetch(:data) == data}
+    # timeframe.find {|thing| thing.fetch(:data) == data}
     dataformat = timeframe.select {|row| row.fetch(:dataformat) == "Percent"}
     data = dataformat.map {|hashes| hashes.fetch(:data)}
     data[0].to_f.round(3)
     # binding.pry
-  end                                                                          # => :free_or_reduced_lunch_in_year, :free_or_reduced_lunch_in_year
+  end
 
-end  # => :free_or_reduced_lunch_in_year, :free_or_reduced_lunch_in_year
+  def free_or_reduced_lunch_by_year
+    timeframe  = data.select{|row| row.fetch(:timeframe) == year.to_s && row.fetch(:poverty_level) == "Eligible for Free or Reduced Lunch"}
+    # timeframe.find {|thing| thing.fetch(:data) == data}
+    dataformat = timeframe.select {|row| row.fetch(:dataformat) == "Percent"}
+    data = dataformat.map {|hashes| hashes.fetch(:data)}
+    binding.pry
+  end
+
+  def school_aged_children_in_poverty_in_year(year)
+    timeframe  = data.select{|row| row.fetch(:timeframe) == year.to_s && row.fetch(:dataformat) == "Percent"}
+    # timeframe.find {|thing| thing.fetch(:data) == data}
+    dataformat = timeframe.select {|row| row.fetch(:dataformat) == "Percent"}
+    data = dataformat.map {|hashes| hashes.fetch(:data)}
+    data[0].to_f.round(3)
+    # binding.pry
+  end
+end
--- test/test_economic_profile.rb
+++ test/test_economic_profile.rb
@@ -8,11 +8,29 @@ require './lib/parse'
 require 'csv'
 
 class TestEconomicProfile < Minitest::Test
+  def test_it_exists
+    assert DistrictRepository
+  end
+
   def test_free_or_reduced_lunch_in_year
     path       = File.expand_path("../data", __dir__)
     repository = DistrictRepository.from_csv(path)
     district   = repository.find_by_name("ACADEMY 20")
-    # require 'pry';binding.pry
     assert_equal 0.125, district.economic_profile.free_or_reduced_lunch_in_year(2012)
   end
+
+  def test_free_or_reduced_lunch_for_different_district
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ASPEN 1")
+    assert_equal 0.066, district.economic_profile.free_or_reduced_lunch_in_year(2012)
+  end
+
+  def test_school_aged_children_in_poverty_in_year
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+    assert_equal 0.064, district.economic_profile.school_aged_children_in_poverty_in_year(2012)
+  end
+
 end
